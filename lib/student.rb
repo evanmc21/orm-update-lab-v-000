cb7576edb9167bcdb294296538da319c6ce5eeb1
Evan Candler <emcandler21@gmail.com>
2018-04-03 01:45:35 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -59,4 +59,12 @@ def self.new_from_db
   Self.new(row[0], row[1], row[2])
 end
 
+def self.find_by_name(name)
+  sql = <<-SQL
+    SELECT * FROM students WHERE name = ? LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db
+    end.first
 end
